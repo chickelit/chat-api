daf6693c331223ed12a75b707b8778112181207a
José Anselmo <anselmojose297@gmail.com>
2021-10-09 10:58:46 -0300

    Update search.spec.ts

--- test/users/search.spec.ts
+++ test/users/search.spec.ts
@@ -30,4 +30,35 @@ test.group("/users/search", async (group) => {
     assert.deepEqual(body.name, user.name);
     assert.deepEqual(body.username, user.username);
   });
+
+  test("should show extra data correctly", async (assert) => {
+    const user = await UserFactory.merge({ password: "secret" }).create();
+    const { token } = await generateToken();
+
+    await request
+      .post(`/users/blocks`)
+      .send({ userId: user.id })
+      .set("authorization", `bearer ${token}`)
+      .expect(200);
+
+    const { body } = await request
+      .get(`/users/search?username=${user.username}`)
+      .set("authorization", `bearer ${token}`)
+      .expect(200);
+
+    assert.exists(body.id);
+    assert.exists(body.email);
+    assert.exists(body.name);
+    assert.exists(body.username);
+    assert.deepEqual(body.id, user.id);
+    assert.deepEqual(body.email, user.email);
+    assert.deepEqual(body.name, user.name);
+    assert.deepEqual(body.username, user.username);
+    assert.exists(body.blocked);
+    assert.exists(body.isBlocked);
+    assert.exists(body.friendship);
+    assert.isFalse(body.blocked);
+    assert.isTrue(body.isBlocked);
+    assert.isFalse(body.friendship);
+  });
 });
