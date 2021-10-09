0d249ec84bd1957b799069c424d41a07fe75867c
José Anselmo <anselmojose297@gmail.com>
2021-10-09 17:16:15 -0300

    Update imports

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -2,3 +2,4 @@ export { default as request } from "./request";
 export { default as generateToken } from "./generateToken";
 export { default as beginRegister } from "./beginRegister";
 export { default as beginPasswordRecovery } from "./beginPasswordRecovery";
+export { default as blockUsers } from "./blockUsers";
