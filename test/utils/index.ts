16588f7eaef97a53a3239411c2cc7171ebc49311
José Anselmo <anselmojose297@gmail.com>
2021-10-10 10:38:03 -0300

    Update imports

--- test/utils/index.ts
+++ test/utils/index.ts
@@ -3,3 +3,4 @@ export { default as generateToken } from "./generateToken";
 export { default as beginRegister } from "./beginRegister";
 export { default as beginPasswordRecovery } from "./beginPasswordRecovery";
 export { default as blockUsers } from "./blockUsers";
+export { default as sendFriendshipRequests } from "./sendFriendshipRequests";
