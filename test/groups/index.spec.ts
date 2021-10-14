6100d838fda80c9c231c430652a434dc28281006
José Anselmo <anselmojose297@gmail.com>
2021-10-14 20:55:52 -0300

    Update index.spec.ts

--- test/groups/index.spec.ts
+++ test/groups/index.spec.ts
@@ -1,6 +1,6 @@
 import Database from "@ioc:Adonis/Lucid/Database";
 import test from "japa";
-import { generateToken, request } from "./utils";
+import { generateToken, request } from "../utils";
 import faker from "faker";
 import { GroupFactory } from "Database/factories/GroupFactory";
 import { UserFactory } from "Database/factories/UserFactory";
@@ -119,7 +119,90 @@ test.group("/groups", async (group) => {
     assert.exists(findGroup);
   });
 
-  /* index */
+  test("[index] - should be able to list authenticated user's groups", async (assert) => {
+    const { user, token } = await generateToken();
 
-  /* show */
+    const groups = await Promise.all(
+      Array(10)
+        .fill(false)
+        .map(async () => {
+          const { body } = await request
+            .post("/groups")
+            .send({
+              title: faker.lorem.words(2)
+            })
+            .set("authorization", `bearer ${token}`)
+            .expect(200);
+
+          return body;
+        })
+    );
+
+    const { body } = await request
+      .get("/groups?page=1&perPage=20")
+      .set("authorization", `bearer ${token}`)
+      .expect(200);
+
+    assert.exists(body.meta);
+    assert.exists(body.data);
+    assert.equal(body.meta.total, groups.length);
+
+    groups.forEach(async (group) => {
+      const memberships = await Database.query()
+        .from("group_members")
+        .where({ user_id: user.id });
+
+      const isValid = memberships.some(
+        (membership) => membership.group_id === group.id
+      );
+
+      assert.isTrue(isValid);
+    });
+  });
+
+  test("[show] - should be able to show an authenticated user's group", async (assert) => {
+    const { user, token } = await generateToken();
+
+    const { body: group } = await request
+      .post("/groups")
+      .send({
+        title: faker.lorem.words(2)
+      })
+      .set("authorization", `bearer ${token}`)
+      .expect(200);
+
+    const { body } = await request
+      .get(`/groups/${group.id}`)
+      .set("authorization", `bearer ${token}`)
+      .expect(200);
+
+    assert.exists(body.id);
+    assert.exists(body.userId);
+    assert.exists(body.title);
+
+    const isMember = await Database.query()
+      .from("group_members")
+      .where({ user_id: user.id, group_id: group.id })
+      .first();
+
+    assert.exists(isMember);
+  });
+
+  test("[show] - should fail when trying to show a group and you are not member of it", async () => {
+    const { token } = await generateToken();
+    const { token: userToken } = await generateToken();
+
+    const { body: group } = await request
+      .post("/groups")
+      .send({
+        title: faker.lorem.words(2)
+      })
+      .set("authorization", `bearer ${token}`)
+      .expect(200);
+
+    await request
+      .get(`/groups/${group.id}`)
+      .set("authorization", `bearer ${userToken}`)
+      .expect(404);
+  });
 });
