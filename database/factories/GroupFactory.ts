283dd92133a8ebd2d9b96270b7df3d6f7a107009
José Anselmo <anselmojose297@gmail.com>
2021-10-14 10:01:09 -0300

    Add GroupFactory.ts

---
+++ database/factories/GroupFactory.ts
@@ -0,0 +1,8 @@
+import Factory from "@ioc:Adonis/Lucid/Factory";
+import { Group } from "App/Models";
+
+export const GroupFactory = Factory.define(Group, ({ faker }) => {
+  return {
+    title: faker.lorem.words(3)
+  };
+}).build();
