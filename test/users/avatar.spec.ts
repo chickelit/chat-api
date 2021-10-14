be1e1fe770b8f6ff77e9da766fdb5a36dea1e73f
José Anselmo <anselmojose297@gmail.com>
2021-10-14 09:28:28 -0300

    Update avatar.spec.ts

--- test/users/avatar.spec.ts
+++ test/users/avatar.spec.ts
@@ -27,4 +27,33 @@ test.group("/users/avatar", async (group) => {
 
     assert.exists(file);
   });
+
+  test("[destroy] - should be able to delete avatar", async (assert) => {
+    const { user, token } = await generateToken();
+
+    await request
+      .put("/users/avatar")
+      .set("authorization", `bearer ${token}`)
+      .attach("file", "test/assets/avatar.jpg")
+      .expect(200);
+
+    const file = await Database.query()
+      .from("files")
+      .where({ file_category: "avatar", user_id: user.id })
+      .first();
+
+    assert.exists(file);
+
+    await request
+      .delete("/users/avatar")
+      .set("authorization", `bearer ${token}`)
+      .expect(200);
+
+    const findFile = await Database.query()
+      .from("files")
+      .where({ file_category: "avatar", user_id: user.id })
+      .first();
+
+    assert.isNull(findFile);
+  });
 });
