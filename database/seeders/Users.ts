0e87cc668ec0a6807d021ff9b0bf2351495f2c0a
José Anselmo <anselmojose297@gmail.com>
2021-12-11 08:32:38 -0300

    Increase the quantity of users on the seeder

--- database/seeders/Users.ts
+++ database/seeders/Users.ts
@@ -4,7 +4,7 @@ import { UserFactory } from "Database/factories/UserFactory";
 
 export default class UserSeeder extends BaseSeeder {
   public async run() {
-    await UserFactory.merge({ password: "secret" }).createMany(60);
+    await UserFactory.merge({ password: "secret" }).createMany(120);
     await User.create({
       email: "me@gmail.com",
       password: "secret",
